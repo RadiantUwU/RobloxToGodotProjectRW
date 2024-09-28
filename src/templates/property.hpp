d8602f7a0b988529c1a20b61bec08951173f5de1
Radiant <69520693+RadiantUwU@users.noreply.github.com>
2024-09-28 23:23:02 +0300

    Update entire property system.

--- src/templates/property.hpp
+++ src/templates/property.hpp
@@ -7,65 +7,125 @@
 
 namespace gdrblx {
 
-#define PROPERTY(p_type, p_name, p_class_name, p_get_f, p_set_f)               \
-    private:                                                                   \
-        friend class __PROPERTY_##p_name;                                      \
-        class __PROPERTY_##p_name {                                            \
-            friend class p_class_name;                                         \
-            __PROPERTY_##p_name (p_class_name* p_s) : s(p_s) {}                \
-            __PROPERTY_##p_name (const __PROPERTY_##p_name& p_o) : s(p_o.s) {} \
-            p_class_name *s;                                                   \
-        public:                                                                \
-            GDRBLX_INLINE p_type operator=(const p_type& p_other) {            \
-                s->p_set_f(p_other);                                           \
-                return *this;                                                  \
-            }                                                                  \
-            GDRBLX_INLINE operator p_type () const {                           \
-                return s->p_get_f();                                           \
-            }                                                                  \
-        };                                                                     \
-    public:                                                                    \
-        __PROPERTY_##p_name p_name = this;
-#define PROPERTY_PTR(p_type, p_name, p_class_name, p_get_f, p_set_f)           \
-    private:                                                                   \
-        friend class __PROPERTY_##p_name;                                      \
-        class __PROPERTY_##p_name {                                            \
-            friend class p_class_name;                                         \
-            __PROPERTY_##p_name (p_class_name* p_s) : s(p_s) {}                \
-            __PROPERTY_##p_name (const __PROPERTY_##p_name& p_o) : s(p_o.s) {} \
-            p_class_name *const s;                                             \
-        public:                                                                \
-            GDRBLX_INLINE p_type operator=(p_type p_other) {                   \
-                s->p_set_f(p_other);                                           \
-                return *this;                                                  \
-            }                                                                  \
-            GDRBLX_INLINE operator p_type () const {                           \
-                return s->p_get_f();                                           \
-            }                                                                  \
-        };                                                                     \
-    public:                                                                    \
-        __PROPERTY_##p_name p_name = this;
+template <typename T>
+class Property {
+protected:
+    using Type = T;
+    void* self;
+    Property(void *p_self) : self(p_self) {}
+    Property(const Property& p_other) : self(p_other.self) {}
 
-#define PROPERTY_READONLY(p_type, p_name, p_class_name, p_get_f)               \
-    private:                                                                   \
-        friend class __PROPERTY_##p_name;                                      \
-        class __PROPERTY_##p_name {                                            \
-            friend class p_class_name;                                         \
-            __PROPERTY_##p_name (const p_class_name* p_s) : s(p_s) {}          \
-            __PROPERTY_##p_name (const __PROPERTY_##p_name& p_o) : s(p_o.s) {} \
-            const p_class_name *const s;                                       \
-        public:                                                                \
-            GDRBLX_INLINE operator p_type () const {                           \
-                return s->p_get_f();                                           \
-            }                                                                  \
-        };                                                                     \
-    public:                                                                    \
-        __PROPERTY_##p_name p_name = this;
+    virtual void set_func(const Type& p_other) = 0;
+    virtual Type get_func() const = 0;
+public:
+    GDRBLX_INLINE Property& operator=(const Type& p_other) {
+        set_func(p_other);
+        return *this;
+    }
+    GDRBLX_INLINE operator Type() const {
+        return get_func();
+    }
+    GDRBLX_INLINE bool operator==(const Type& p_other) const {
+        return get_func() == p_other;
+    }
+    GDRBLX_INLINE bool operator!=(const Type& p_other) const {
+        return get_func() != p_other;
+    }
+    GDRBLX_INLINE auto operator->() const requires requires (Type o) {o.operator->();}{
+        return get_func();
+    }
+    GDRBLX_INLINE auto operator*() const requires requires (Type o) {*o;} {
+        return *get_func();
+    }
 
-#define PROPERTY_READONLY_PROXY(p_type, p_name, p_class_name, p_private_name)             \
-    private:                                                                              \
-        GDRBLX_INLINE p_type __PROPERTY_##p_name##_GET () const {return p_private_name;}  \
-        PROPERTY_READONLY(p_type, p_name, p_class_name, __PROPERTY_##p_name##_GET)        \
+    GDRBLX_INLINE auto operator+(const Type& p_other) const requires requires (Type o) {o+o;} {
+        return get_func()+p_other;
+    }
+    GDRBLX_INLINE auto operator-(const Type& p_other) const requires requires (Type o) {o-o;} {
+        return get_func()-p_other;
+    }
+    
+};
+template <typename T> requires std::is_reference_v<T>
+class Property<T> {
+protected:
+    using Type = std::remove_reference_t<T>;
+
+    void* self;
+    Property(void *p_self) : self(p_self) {}
+    Property(const Property& p_other) : self(p_other.self) {}
+
+    virtual void set_func(const Type& p_other) = 0;
+    virtual Type& get_func() const = 0;
+public:
+    GDRBLX_INLINE Property& operator=(const Type& p_other) {
+        set_func(p_other);
+        return *this;
+    }
+    GDRBLX_INLINE operator Type() const {
+        return get_func();
+    }
+    GDRBLX_INLINE operator Type&() const {
+        return get_func();
+    }
+    GDRBLX_INLINE bool operator==(const Type& p_other) const {
+        return get_func() == p_other;
+    }
+    GDRBLX_INLINE bool operator!=(const Type& p_other) const {
+        return get_func() != p_other;
+    }
+    GDRBLX_INLINE auto operator->() const requires requires (Type o) {o.operator->();}{
+        return get_func();
+    }
+    GDRBLX_INLINE auto operator*() const requires requires (Type o) {*o;} {
+        return *get_func();
+    }
+    GDRBLX_INLINE const Type* operator&() const {
+        return &get_func();
+    }
+
+    GDRBLX_INLINE auto operator+(const Type& p_other) const requires requires (Type o) {o+o;} {
+        return get_func()+p_other;
+    }
+    GDRBLX_INLINE auto operator-(const Type& p_other) const requires requires (Type o) {o-o;} {
+        return get_func()-p_other;
+    }
+    
+};
+
+#define PROPERTY(p_type, p_name, p_class_name, p_get_f, p_set_f)                                                         \
+    private:                                                                                                             \
+        friend class __PROPERTY_##p_name;                                                                                \
+        class __PROPERTY_##p_name : public Property<p_type> {                                                            \
+            friend class p_class_name;                                                                                   \
+            __PROPERTY_##p_name (p_class_name* p_s) : Property(p_s) {}                                                   \
+        protected:                                                                                                       \
+            GDRBLX_INLINE void set_func(const Type& p_other) override {((p_class_name*)this->self)->p_set_f(p_other);}   \
+            GDRBLX_INLINE p_type get_func() const override {return ((p_class_name*)this->self)->p_get_f();}              \
+            GDRBLX_INLINE Property& operator=(const Type& p_other) {                                                     \
+                set_func(p_other);                                                                                       \
+                return *this;                                                                                            \
+            }                                                                                                            \
+        };                                                                                                               \
+    public:                                                                                                              \
+        __PROPERTY_##p_name p_name = this;
+#define PROPERTY_READONLY(p_type, p_name, p_class_name, p_get_f)                                             \
+    private:                                                                                                 \
+        friend class __PROPERTY_##p_name;                                                                    \
+        class __PROPERTY_##p_name : public Property<p_type> {                                                \
+            friend class p_class_name;                                                                       \
+            __PROPERTY_##p_name (p_class_name* p_s) : Property(p_s) {}                                       \
+        protected:                                                                                           \
+            GDRBLX_INLINE void set_func(const Type& p_other) override {}                                     \
+            GDRBLX_INLINE p_type get_func() const override {return ((p_class_name*)this->self)->p_get_f();}  \
+        };                                                                                                   \
+    public:                                                                                                  \
+        const __PROPERTY_##p_name p_name = this;
+
+#define PROPERTY_READONLY_PROXY(p_type, p_name, p_class_name, p_private_name)                    \
+    private:                                                                                     \
+        GDRBLX_INLINE const p_type& __PROPERTY_##p_name##_GET () const {return p_private_name;}  \
+        PROPERTY_READONLY(const p_type&, p_name, p_class_name, __PROPERTY_##p_name##_GET)         
         
 
 }; // namespace gdrblx
